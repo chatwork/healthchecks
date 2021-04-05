847bd1dd190206e16d9cb588c885ed889ec390c9
TATSUNO Yasuhiro <ytatsuno.jp@gmail.com>
2021-04-05 10:25:54 +0900

    Bump akka-http to support Scala 2.13

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Version {
   val Scala = Seq("2.11.12", "2.12.6", "2.13.5")
   val circe = "0.9.3"
   val akka = "2.5.16"
-  val akkaHttp = "10.1.4"
+  val akkaHttp = "10.1.14"
   val akkaHttpCirce = "1.21.0"
   val cats = "1.2.0"
   val scalaTest = "3.0.5"
